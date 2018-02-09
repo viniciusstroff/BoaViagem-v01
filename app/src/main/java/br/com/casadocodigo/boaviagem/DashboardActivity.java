c5c4e36a6b0400751d85483e4abb09edcd885d34
Vinicius Stroff <viniciusstroff@hotmail.com>
2018-02-09 21:27:34 -0200

    Initial commit

---
+++ app/src/main/java/br/com/casadocodigo/boaviagem/DashboardActivity.java
@@ -0,0 +1,50 @@
+package br.com.casadocodigo.boaviagem;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+
+/**
+ * Created by Vinicius' on 11/01/2018.
+ */
+
+public class DashboardActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+    public void selecionarOpcao(View view) {
+        switch (view.getId()) {
+            case R.id.nova_viagem:
+                startActivity(new Intent(this, ViagemActivity.class));
+                break;
+            case R.id.novo_gasto:
+                startActivity(new Intent(this, GastoActivity.class));
+                break;
+            case R.id.minhas_viagens:
+                startActivity(new Intent(this, ViagemListActivity.class));
+                break;
+            case R.id.configuracoes:
+                // ainda nao implementado
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dashboard_menu, menu);
+        return true;
+
+    }
+    public boolean onMenuItemSelected(int featureId, MenuItem item){
+        finish();
+        return true;
+    }
+}
---
+++ app/src/main/java/br/com/casadocodigo/boaviagem/DatabaseHelper.java
@@ -0,0 +1,8 @@
+package br.com.casadocodigo.boaviagem;
+
+/**
+ * Created by Vinicius' on 16/01/2018.
+ */
+
+public class DatabaseHelper {
+}
